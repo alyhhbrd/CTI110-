b7248dafc33c5308403776364d134b84e92590f3
alyhhbrd <alyhhbrd@gmail.com>
2019-02-16 13:30:47 -0500

    Add files via upload
    first submission

---
+++ P2T1_SalesPrediction_AaliyahHubbard.py
@@ -0,0 +1,14 @@
+# Programming Exercise 2 - Sales Prediction
+# 02162019
+# CTI-110 P2T1 - Sales Prediction
+# Aaliyah Hubbard
+#
+
+# get projected total sales:
+total_sales = float(input('Enter the projected total sales: '))
+
+# calculate profit as 23% of total:
+profit = total_sales * 0.23
+
+# display profit:
+print('The profit is $', format(profit, ',.2f'))
